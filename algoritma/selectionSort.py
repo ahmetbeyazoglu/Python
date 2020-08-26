88aa7c4367bb7740f93e73a4475e702fb9f4194a
Herpestes0 <46286059+AhmetBeyazoglu@users.noreply.github.com>
2020-08-26 12:24:27 +0300

    Add files via upload

---
+++ algoritma/selectionSort.py
@@ -0,0 +1,15 @@
+
+a = int(input("Kaç tane sayı gireceksiniz?: "))
+dizi = []
+for i in range(a):
+    dizi.append(int(input()))
+for i in range(len(dizi)):
+    min = i
+    for j in range(i + 1, len(dizi)):
+        if dizi[min] > dizi[j]:
+            min = j
+    dizi[i], dizi[min] = dizi[min], dizi[i]
+
+print("Sıralanmış dizi: ")
+for i in range(len(dizi)):
+    print(dizi[i])
